133fd4bec4cdbc9ddc268f77588705acb51c345b
Jessie Odeh <jessieodeh08@gmail.com>
2024-03-31 15:04:39 +0200

    - add hex conversion

--- src/pages/tools/ASCII.tsx
+++ src/pages/tools/ASCII.tsx
@@ -1,17 +1,18 @@
-import { For, createSignal } from "solid-js";
+import { createSignal } from "solid-js";
 import {
   InputContainer,
   OutputContainer,
   ToolContainer,
 } from "../../components/tool-containers";
-
 export default function ASCIIPage() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
   const [text, setText] = createSignal("");
-  const encodedText = () =>
+  const decimalEncodedText = () =>
     text()
       .split("")
       .map((char) => char.charCodeAt(0));
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const hexEncodedText = () =>
+    decimalEncodedText().map((char) => "0x" + char.toString(16));
   return (
     <>
       <h1>Text to Number</h1>
@@ -28,9 +29,15 @@ export default function ASCIIPage() {
         </InputContainer>
         <OutputContainer>
           <div>
-            <h3>ASCII Result:</h3>
+            <h3>ASCII Decimal Result:</h3>
             <p class="border border-black p-2 container">
-              {encodedText().join(" ")}
+              {decimalEncodedText().join(" ")}
+            </p>
+          </div>
+          <div>
+            <h3>ASCII Hexadecimal Result:</h3>
+            <p class="border border-black p-2 container">
+              {hexEncodedText().join(" ")}
             </p>
           </div>
           <div>
